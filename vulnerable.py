80b827969f32619c62362289cd51c686193392bc
jayashri1983 <jayashri.phonde@dmce.ac.in>
2025-08-05 19:12:29 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -2,7 +2,7 @@ import os
 
 def run_command():
     user_input = input("Enter command: ")
-    os.system(user_input)  # 🚨 Vulnerability: Command injection
+    os.system(user_input)  # 🚨 Vulnerable: Command Injection
 
 if __name__ == "__main__":
     run_command()
